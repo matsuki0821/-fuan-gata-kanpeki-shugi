e9dd3ab120dc546501d59d28d5c00239b0734910
matsuki0821 <akksmmr26@gmail.com>
2025-04-12 22:04:13 +0900

    fix: correct stylesheet path

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,15 @@ import { defineConfig } from 'vite';
 import path from 'path';
 
 export default defineConfig({
+  base: './',
   build: {
     outDir: 'dist',
     rollupOptions: {
       input: {
         main: path.resolve(__dirname, 'pages/index.html'),
         privacy: path.resolve(__dirname, 'pages/privacy-policy.html'),
-      }
-    }
-  }
+      },
+    },
+  },
 });
+
